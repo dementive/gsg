75c6cbb9e9461aacf60bfbdf7dc8538bda569789
dementive <87823030+dementive@users.noreply.github.com>
2025-06-03 00:15:41 -0400

    entt test

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,5 +1,6 @@
 #include "register_types.h"
 
+#include "core/string/print_string.h"
 #include "nodes/Map3D.hpp"
 #include "templates/SingletonAllocator.hpp"
 #include "data/Province.hpp"
@@ -7,6 +8,8 @@
 #include "data/Region.hpp"
 #include "data/Country.hpp"
 #include "data/Locator.hpp"
+
+#include <entt/entity/registry.hpp>
 
 using namespace CG;
 
@@ -26,11 +29,56 @@ ProvinceLocator
 }
 // clang-format on
 
+struct Name : String {};
+
+using TerritoryEntity = entt::entity;
+
+using NationEntity = entt::entity;
+
+static void update(entt::registry &registry) {
+    auto view = registry.view<const Name>();
+
+    for(auto [entity, name]: view.each()) {
+    	print_line(vformat("Entity: %d name: %s", static_cast<int>(entity), name));
+    	//registry.destroy(entity);
+    	// view.get<Name>(entity);
+    }
+}
+
+// Stores the Entity ids of every entity type and wraps entt::registry.
+struct EntityRegistry {
+	Vec<TerritoryEntity> territories;
+	entt::registry registry;
+};
+
 void initialize_src_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
 		return;
 
 	singleton_allocator.init();
+
+	EntityRegistry entity_registry;
+	entt::registry registry;
+	entity_registry.territories.resize(10);
+
+	// create provinces
+	for(int i = 0; i < 10; ++i) {
+		const TerritoryEntity entity = registry.create();
+		registry.emplace<Name>(entity, "Ohio");
+		entity_registry.territories[i] = entity;
+	}
+
+	// create nations
+	for(int i = 0; i < 10; ++i) {
+		const NationEntity entity = registry.create();
+		registry.emplace<Name>(entity, "Rome");
+	}
+
+	for (const TerritoryEntity entity: entity_registry.territories) {
+		print_line(vformat("Territory Entity: %d name: %s", static_cast<int>(entity), registry.get<Name>(entity)));
+	}
+
+	update(registry);
 
 	GDREGISTER_RUNTIME_CLASS(Map3D)
 }
