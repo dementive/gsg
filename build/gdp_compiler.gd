2dd84859756ee48a9c97eb62e4934e730ddc76a1
dementive <87823030+dementive@users.noreply.github.com>
2025-07-18 03:20:25 -0500

    Preprocessor stuff

--- build/gdp_compiler.gd
+++ build/gdp_compiler.gd
@@ -16,14 +16,24 @@ var static_type_pattern: RegEx
 var static_type_no_equals_pattern: RegEx
 var type_no_equals_pattern: RegEx
 var typed_for_pattern: RegEx
+var type_alias_pattern: RegEx
+var macro_pattern: RegEx
 
 var is_inside_open_paren: bool = false
 var is_compilation_enabled: bool = true
 
+var macro_line_continuation: bool = false
+var macro_line_continuation_first: bool = false
+
+var current_macro_value: String = ""
+var current_macro_key: String = ""
+
 var is_compilation_enabled_stack: Array[bool] = []
 
+var macro_dict: Dictionary[String, Variant] = {}
 var constexpr_variables: Dictionary[String, Variant] = {}
 var consteval_variables: Dictionary[String, Variant] = {}
+var type_aliases: Dictionary[String, String] = {}
 
 func whitespace_split(p_string: String, p_splitter: String = " ") -> PackedStringArray:
 	var ret: PackedStringArray
@@ -103,21 +113,23 @@ func compile_regex() -> void:
 	typed_for_pattern = RegEx.new()
 	typed_for_pattern.compile(r"for\s+{type_pattern}\s+{var_pattern}".format({"type_pattern": type_pattern, "var_pattern": var_pattern}))
 
+	type_alias_pattern = RegEx.new()
+	type_alias_pattern.compile(r"using\s+{type_pattern}\s+=\s+{type_pattern}".format({"type_pattern": type_pattern}))
+
 func exec_and_return(expression: String) -> Variant: 
 	var expr := Expression.new()
 	expr.parse(expression)
-	return expr.execute()
-
-func write_array(array: PackedStringArray, file: FileAccess) -> void:
-	var line: String = " ".join(array)
-	file.store_line(line)
+	var result: Variant = expr.execute() 
+	if (expr.has_execute_failed()):
+		print("Error executing expression: ", expression)
+	return result
 
 func replace_consteval_vars(line_tokens: PackedStringArray) -> PackedStringArray:
 	for variable in consteval_variables:
 		var iter_idx: int = 0
 		for token in line_tokens:
 			if token == variable:
-				line_tokens[iter_idx] = consteval_variables[variable]
+				line_tokens[iter_idx] = type_convert(consteval_variables[variable], TYPE_STRING)
 			iter_idx += 1
 
 	return line_tokens
@@ -127,14 +139,10 @@ func replace_constexpr_vars(line_tokens: PackedStringArray) -> PackedStringArray
 		var iter_idx: int = 0
 		for token in line_tokens:
 			if token == variable:
-				line_tokens[iter_idx] = constexpr_variables[variable]
+				line_tokens[iter_idx] = type_convert(constexpr_variables[variable], TYPE_STRING)
 			iter_idx += 1
 
 	return line_tokens
-
-func get_regex_groups(regex: RegEx, pattern: String) -> PackedStringArray:
-	var constexpr_var_match: RegExMatch = regex.search(pattern)
-	return constexpr_var_match.strings
 
 func get_indentation_level(line: String) -> int:
 	var level: int = 0
@@ -147,21 +155,60 @@ func get_indentation_level(line: String) -> int:
 	return level
 
 func process_line(line: String, file: FileAccess) -> void:
+	if macro_line_continuation:
+		if macro_line_continuation_first:
+			macro_line_continuation_first = false
+			line = line.lstrip("\t")
+		if not line.is_empty():
+			current_macro_value += line + "\n"
+		if not line.ends_with("\\"):
+			macro_line_continuation = false
+			macro_dict[current_macro_key] = current_macro_value.replace("\\", "")
+			current_macro_value = ""
+		return
+
+	if line.begins_with("#define"):
+		var macro_tokens: PackedStringArray = line.split(" ")
+		macro_tokens.remove_at(0)
+		current_macro_key = macro_tokens[0]
+		current_macro_value = macro_tokens[1]
+
+		if current_macro_value.ends_with("\\"):
+			macro_line_continuation = true
+			macro_line_continuation_first = true
+		elif not current_macro_value.is_empty():
+			macro_dict[current_macro_key] = current_macro_value
+
+		return
+
 	var indentation_level: int = get_indentation_level(line)
 	line = line.strip_edges()
 	var line_tokens: PackedStringArray = whitespace_split(line)
-	if (line_tokens.is_empty()):
-		file.store_line("")
+
+	# Replace consteval variables with their values
+	line_tokens = replace_consteval_vars(line_tokens)
+	for variable in consteval_variables:
+		line = line.replacen(variable, type_convert(consteval_variables[variable], TYPE_STRING))
+
+	if line.begins_with("#if"):
+		line_tokens = replace_constexpr_vars(line.split(" "))
+		line_tokens.remove_at(0)
+
+		var expression: String = " ".join(line_tokens)
+		var expression_result = exec_and_return(expression)
+		if (expression_result is bool or expression_result is int):
+			is_compilation_enabled = expression_result
+			is_compilation_enabled_stack.append(is_compilation_enabled)
+		else:
+			print("Wrong type for #if preprocessor statement")
+
 		return
 
-	var first_token: String = line_tokens[0]
-	var last_token: String = line_tokens[line_tokens.size() - 1]
-
-	if first_token == "#else":
+	if line.begins_with("#else"):
 		is_compilation_enabled = not is_compilation_enabled
 		return
 
-	if first_token == "#endif":
+	if line.begins_with("#endif"):
 		if is_compilation_enabled_stack.size() > 0:
 			is_compilation_enabled = is_compilation_enabled_stack.pop_back()
 		else:
@@ -171,6 +218,47 @@ func process_line(line: String, file: FileAccess) -> void:
 			is_compilation_enabled = true
 
 		return
+
+	if line.begins_with("using"):
+		var alias_var_match: RegExMatch = type_alias_pattern.search(line)
+		if alias_var_match:
+			var groups: PackedStringArray = alias_var_match.strings
+			type_aliases[groups[1]] = groups[2]
+		else:
+			print("Invalid type alias on line: ", line)
+			print_stack()
+
+		return
+
+	if line.begins_with("#"):
+		file.store_line(line)
+		return
+
+
+	var iter_idx: int = 0
+	for token in line_tokens:
+		for alias in type_aliases:
+			if token == alias:
+				line_tokens[iter_idx] = type_convert(type_aliases[alias], TYPE_STRING)
+
+		for macro in macro_dict:
+			if token == macro:
+				line_tokens[iter_idx] = type_convert(macro_dict[macro], TYPE_STRING)
+		
+		iter_idx += 1
+
+
+	var line_split: PackedStringArray = line.split("#")
+	var comment_string: String = ""
+	if line_split.size() > 1:
+		comment_string = line_split[1]
+
+	if (line_tokens.is_empty()):
+		file.store_line("")
+		return
+
+	var first_token: String = line_tokens[0]
+	var last_token: String = line_tokens[line_tokens.size() - 1]
 
 	if not is_compilation_enabled:
 		return
@@ -182,58 +270,55 @@ func process_line(line: String, file: FileAccess) -> void:
 	if (first_token == "static" and line_tokens[1] == "fn"):
 		line_tokens[1] = "func"
 
-	# Replace consteval variables with their values
-	line_tokens = replace_consteval_vars(line_tokens)
-
 	# Constexpr variables
 	if first_token == "constexpr":
 		line_tokens = replace_constexpr_vars(line_tokens)
-
 		var line_str: String = " ".join(line_tokens)
-		var groups: PackedStringArray = get_regex_groups(constexpr_var_pattern, line_str)
-		if groups.size() > 0:
+
+		var constexpr_var_match: RegExMatch = constexpr_var_pattern.search(line_str)
+		if !constexpr_var_match:
+			print("Compile error on line: ", line)
+			print_stack()
 			return
 
+		var groups: PackedStringArray = constexpr_var_match.strings
 		var var_name: String = groups[2]
 		var expression: String = groups[3].strip_edges()
 		var expression_result: Variant = exec_and_return(expression)
 		constexpr_variables[var_name] = expression_result
 
 		var idx: int = 0
+		var should_replace: bool = false
+		var new_tokens: PackedStringArray
 		for token in line_tokens:
-			token = token.replace(expression, str(expression_result))
+			if !should_replace and token == "=":
+				should_replace = true
+				new_tokens.append(token)
 			if token == "constexpr":
 				token = "const"
-			line_tokens[idx] = token
+			if !should_replace:
+				new_tokens.append(token)
 
 			idx += 1
+
+		line_tokens = new_tokens
+		line_tokens.append(str(expression_result))
 
 	if first_token == "consteval":
 		line_tokens = replace_constexpr_vars(line_tokens)
 
 		var line_str: String = " ".join(line_tokens)
-		var groups: PackedStringArray = get_regex_groups(consteval_var_pattern, line_str)
-		if groups.size() > 0:
+		var consteval_var_match: RegExMatch = consteval_var_pattern.search(line_str)
+		if !consteval_var_match:
+			print("Compile error on line: ", line)
+			print_stack()
 			return
 
+		var groups: PackedStringArray = consteval_var_match.strings
 		var var_name: String = groups[2]
 		var expression: String = groups[3].strip_edges()
 		var expression_result: Variant = exec_and_return(expression)
 		consteval_variables[var_name] = expression_result
-		return
-
-	if first_token == "#if":
-		line_tokens = replace_constexpr_vars(line_tokens)
-		var pp_tokens: PackedStringArray = line_tokens
-		pp_tokens.remove_at(0)
-
-		var expression: String = " ".join(pp_tokens)
-		var expression_result: Variant = exec_and_return(expression)
-		if (expression_result is bool or expression_result is int):
-			is_compilation_enabled = expression_result
-			is_compilation_enabled_stack.append(is_compilation_enabled)
-		else:
-			print("Wrong type for preprocessor statement")
 
 		return
 
@@ -244,53 +329,15 @@ func process_line(line: String, file: FileAccess) -> void:
 		is_inside_open_paren = false
 
 	var line_str = " ".join(line_tokens)
-	var comment_string: String = ""
-	var line_split: PackedStringArray = line_str.split("#")
-	var split_line: String = line_split[0]
-
-	# Make it so you don't need to use colons when the compiler can figure out where one should go
-	if (
-		line_str.begins_with("static func")
-		or first_token == "func"
-		or first_token == "for"
-		or first_token == "while"
-		or first_token == "class"
-		or first_token == "match"
-		or first_token == "else"
-	) and not (last_token == ':' or first_token == "class_name" or is_inside_open_paren):
-		if line_split.size() > 1:
-			comment_string = "#" + line_split[1]
-		line_str = split_line.substr(0, split_line.length()) + ": " + comment_string + split_line.substr(split_line.length(), 1)
-		line_tokens = whitespace_split(line_str)
-
-	line_split = line_str.split("#")
-	split_line = line_split[0]
-
-	if not last_token == "\\" and not is_inside_open_paren and first_token == "if" or first_token == "elif":
-		if not " ".join(line_tokens).contains(":"):
-			comment_string = ""
-			if line_split.size() > 1:
-				comment_string = "#" + line_split[1]
-			line_str = split_line.substr(0, split_line.length()) + ": " + comment_string + split_line.substr(split_line.length(), 1)
-			line_tokens = whitespace_split(line_str)
-
-	line_split = line_str.split("#")
-	split_line = line_split[0]
 
 	# Typed function params
 	if first_token == "func":
-		if line_split.size() > 1:
-			comment_string = "#" + line_split[1]
-
 		var no_equals_match: Array[RegExMatch] = static_type_func_no_equals_pattern.search_all(line_str)
 		if no_equals_match.size() > 1:
 			for match: RegExMatch in no_equals_match:
 				var groups: PackedStringArray = match.strings
 				if not groups[1].begins_with("func") and not groups[1].begins_with("static"):
-					split_line = split_line.replace("%s %s" % [groups[1], groups[2]], "%s: %s" % [groups[2], groups[1]])
-
-			line_str = split_line.substr(0, split_line.length()) + comment_string + split_line.substr(split_line.length(), 1)
-			line_tokens = whitespace_split(line_str)
+					line_str = line_str.replace("%s %s" % [groups[1], groups[2]], "%s: %s" % [groups[2], groups[1]])
 
 	var pattern_match: RegExMatch = memnew_pattern.search(line_str)
 	if pattern_match:
@@ -342,6 +389,9 @@ func process_line(line: String, file: FileAccess) -> void:
 	for level in range(indentation_level):
 		line_str = "\t" + line_str
 
+	if comment_string:
+		line_str += " #" + comment_string
+
 	file.store_line(line_str)
 
 
@@ -349,9 +399,21 @@ func compile(input_file_name: String, output_file_name: String) -> void:
 	var file := FileAccess.open(input_file_name, FileAccess.READ)
 	var out_file := FileAccess.open(output_file_name, FileAccess.WRITE)
 	while not file.eof_reached():
-		process_line(file.get_line(), out_file)
+		var line: String = file.get_line()
+
+		if line.strip_edges().begins_with("#include"):
+			var include_path: PackedStringArray = line.split("#include")
+			var include_path_str: String = include_path[1].strip_edges().replace('"', "")
+
+			var include_file := FileAccess.open(include_path_str, FileAccess.READ)
+			line = include_file.get_as_text()
+			var lines: PackedStringArray = line.split("\n")
+			for included_line in lines:
+				process_line(included_line, out_file)
+
+		process_line(line, out_file)
 	file.close()
 
 func _init(): 
 	compile_regex()
-	compile("simple_test.gdp", "output.gd")
+	compile("test.gdp", "output.gd")
--- build/output.gd
+++ build/output.gd
@@ -4,12 +4,12 @@
 class_name MyClass
 
 # Inheritance:
-extends BaseClass
+extends Node
 
 
 
-func global_function_test() -> void: 
-	print("Hello World!")
+func global_function_test() -> void:
+	print("Hello World!") # Hi
 
 
 @export var int_number: int = 5
@@ -19,12 +19,14 @@ func global_function_test() -> void:
 @onready var health_bar: ProgressBar = get_node("UI/LifeBar")
 
 # Member variables.
-var a = 5
+var a = 5 # hmm
 var s = "Hello"
 var arr = [1, 2, 3]
 var dict = {"key": "value", 2: 3}
 
-var other_dict: Dictionary[String, int] = {key = "value": other_key = 2}
+var other_dict: Dictionary[String, int] = {"1": 22, "3": 2}
+
+var another_dict: Dictionary[String, String] = {"1": "22", "3": "2"}
 
 var typed_var: int
 var static_typed_var: int
@@ -39,34 +41,49 @@ static var new_typed_var_2: int = 0
 static var inferred_type_2 := "String"
 
 const new_typed_var_23: int = 0
-static const var new_typed_var_234: int = 0
+static var new_typed_var_234: int = 0
 
-const constexpr_test: float = 1.4142135623730951
-const constexpr_test2: float = 2.8284271247461903
+const constexpr_test: float = 1.4142135623731
+const constexpr_test2: float = 2.8284271247462
 
-func constexpr_usage_test_func(x: int) -> void:: 
+var really_long_variable_name_that_is_very_long: int = 0
+
+
+func macro_func() -> void:
+	var x: int = 999 
+	for i: int in range(x): 
+		print(i) 
+	var y = 9999
+
+	print(x - y)
+
+func macro_func2() -> void:
+	var x: int = 999 
+	for i: int in range(x): 
+		print(i) 
+	var y = 9999
+
+	print(x + y)
+
+func constexpr_usage_test_func(x: int) -> int:
 	var value: int = constexpr_test2 + x
 	return value
 
 
-func consteval_usage_test_func(x: int) -> void:: 
-	var value: int = 1.4142135623730951 + x
+func consteval_usage_test_func(x: int) -> int:
+	var value: int = 4096.0 + x
 	return value
 
-const constexpr_test_str: String = "Square root of 8: 2.8284271247461903\nSquare root of 12: 3.4641016151377544"
-
-const TWO_PI: float = 1.5707963267948966
-
 # Conditional compilation with constant expressions
-
-func super_secret_debug_function() -> void: 
-	print("I like cats")
+func normal_non_debug_function() -> void:
+	print("I like dogs")
 
 const cond_comp_var: int = 0
 const cond_comp_var_two: int = 4
 
-func get_favorite_animal() -> void: 
-	return "Frogs are my favorite"
+
+func get_favorite_animal() -> String:
+	return "Rabbits are cool"
 
 # Constants.
 const ANSWER: int = 42
@@ -77,68 +94,47 @@ const ANSWER_2: int = 999
 enum {UNIT_NEUTRAL, UNIT_ENEMY, UNIT_ALLY}
 enum Named {THING_1, THING_2, ANOTHER_THING = -1}
 
-# multiline comments?!
-# Built-in vector types.
-# var v2 := Vector2(1, 2)
-# var v3 := Vector3(1, 2, 3)
-# var v4 := Vector4(1, 2, 3, 4)
-# they work here too? 
-
-# 	Whoa
-# 	There
-
-## The description of the variable. Using multi line doc comments works!
-## 
-## More words.
-## 
-## @deprecated: Use [member other_var] instead.
-var test_doc_comment_var: int = 500
-
-func some_function(param1: int, param2: int, param3: int) -> int: 
+func some_function(param1: int, param2: int, param3: int) -> int:
 	const local_const = 5
 	var is_thing_enabled: bool = false
 
-	if param1 < local_const: # Don't need a : at the end of the line.
- 		print(param1)
-	elif param2 > 5: 
+	if param1 < local_const:
+		print(param1)
+	elif param2 > 5:
 		print(param2)
-	else: 
+	else:
 		print("Fail!")
 
-	for i in range(20): 
+	for i in range(20):
 		print(i)
 
-	for i: int in range(10): 
+	for i: int in range(10):
 		print(i)
 
-	# Single and multiline if statements need a ":"
-	if 1 > 0: return
+	if param1 < local_const and 1 > 0:
+		print(param1)
 
-	if param1 < local_const \
-		1 > 0:
-			print(param1)
-
-	next_state = "idle" if true != false else "fall"
+	var next_state = "idle" if true != false else "fall"
 
 	var angle_degrees: int = 135
 	var quadrant: String = (
-			"northeast" if angle_degrees <= 90
-			else "southeast" if angle_degrees <= 180
-			else "southwest" if angle_degrees <= 270
-			else "northwest"
+		"northeast" if angle_degrees <= 90
+		else "southeast" if angle_degrees <= 180
+		else "southwest" if angle_degrees <= 270
+		else "northwest"
 	)
 
 	var position := Vector2(250, 350)
 	if (
-			position.x > 200 and position.x < 400
-			and position.y > 300 and position.y < 400
+		position.x > 200 and position.x < 400
+		and position.y > 300 and position.y < 400
 	):
 		pass
 
-	while param2 != 0: 
+	while param2 != 0:
 		param2 -= 1
 
-	match param3: 
+	match param3:
 		3:
 			print("param3 is 3!")
 		_:
@@ -148,23 +144,23 @@ func some_function(param1: int, param2: int, param3: int) -> int:
 	return local_var
 
 
-func something(p1: int, p2: int): 
-	super(p1, p2)
+# fn something(int p1, p2: int):
+# 	super(p1, p2)
 
 
-func other_something(p1: String = "Hello", p2: int = 50): 
-	super.something(p1, p2)
+# fn other_something(String p1 = "Hello", int p2 = 50):
+# 	super.something(p1, p2)
 
-func test_thing(p_thing: int = 1, vec_thing := Vector2(0)) -> float: 
+func test_thing(p_thing: int = 1, vec_thing := Vector2(0, 0)) -> float:
 	return 1.0
 
-static func test_static() -> void: 
+static func test_static() -> void:
 	pass
 
-class Item: 
+class Item:
 	var a: int = 10
 
-func add(reference: Item, amount: int) -> Item: 
+func add(reference: Item, amount: int) -> Item:
 	reference.add(amount)
 	return reference
 
@@ -173,9 +169,9 @@ var items: Array[Item] = [Item.new()]
 var array_of_arrays: Array[Array] = [[], []]
 
 # Constructor
-func _init(): 
+func _init():
 	print("Constructed!")
-	var lv = Something.new()
+	var lv = Object.new()
 	print(lv.a)
 
 	var custom_node: Node = Node.new()
@@ -183,3 +179,4 @@ func _init():
 
 	var obj: Object = Object.new()
 	obj.free()
+
